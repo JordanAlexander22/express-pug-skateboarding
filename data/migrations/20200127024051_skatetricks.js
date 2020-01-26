7a99e21b629174a53178194c75f70229740aa928
jordan <jordan@jordans-MacBook-Air.local>
2020-01-27 02:44:46 +0700

    added trick migrations and created multi table list

---
+++ data/migrations/20200127024051_skatetricks.js
@@ -0,0 +1,64 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("tricks", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.string("description");
+      tbl
+        .boolean("learned")
+        .notNullable()
+        .defaultTo(true);
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl.string("name").notNullable();
+      tbl.string("description");
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.string("description").notNullable();
+      tbl.string("notes");
+      tbl
+        .boolean("completed")
+        .notNullable()
+        .defaultTo(false);
+      // foreign key
+      tbl
+        .integer("trick_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("tricks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("trick_resources", tbl => {
+      tbl.increments();
+      // foreign key
+      tbl
+        .integer("trick_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("tricks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      // foreign key
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("tricks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("trick_resources");
+};
