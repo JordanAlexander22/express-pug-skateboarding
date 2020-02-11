024c14a0b4ac7077f4a9056c021302d08e389b47
jordan <jordan@jordans-MacBook-Air.local>
2020-02-12 01:55:56 +0700

    iniated react

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require("./api/server");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 server.listen(port, () =>
   console.log(`server up and running on port: ${port}`)
 );
