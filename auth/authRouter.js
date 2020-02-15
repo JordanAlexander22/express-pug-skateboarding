91c8e1f4be9294d8a6949bb2a4dc248e45946026
jordan <jordan@jordans-MacBook-Air.local>
2020-02-16 01:00:11 +0700

    debugged deleting tasks in auth router

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -5,7 +5,7 @@ const { newToken } = require("./authMid");
 const Users = require("../users/user-model");
 const Tricks = require("../tricks/tricks-model");
 const Tasks = require("../tasks/task-model");
-
+const Resources = require("../resources/resource-model");
 
 //Login and Register
 router.post("/register", (req, res) => {
@@ -32,7 +32,7 @@ router.post("/login", (req, res) => {
   Users.findby({ username })
     .first()
     .then(user => {
-    //   console.log(user.password);
+      //   console.log(user.password);
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = newToken(Users);
         res.status(200).json({
@@ -49,54 +49,86 @@ router.post("/login", (req, res) => {
     });
 });
 
-// need to be logged in to add a trick 
+// need to be logged in to add a trick
 // ADD a trick
 router.post("/tricks", async (req, res) => {
-    const trickData = req.body;
-  
-    if (!req.body)
-      return res.status(400).json({
-        errorMessage: "Please provide required info."
-      });
-    try {
-      const trick = await Tricks.add(trickData);
-      res.status(201).json(trick);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to create new trick" });
-    }
-  });
+  const trickData = req.body;
+
+  if (!req.body)
+    return res.status(400).json({
+      errorMessage: "Please provide required info."
+    });
+  try {
+    const trick = await Tricks.add(trickData);
+    res.status(201).json(trick);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create new trick" });
+  }
+});
 
 //    post a task to a trick
-//    make sure to also include trick_id in the request        
+//    make sure to also include trick_id in the request
 router.post("/:id/tasks", (req, res) => {
-    Tricks.addTasks(req.body, req.params.id)
-      .then(task => {
-        res.status(201).json(task);
-      })
-      .catch(err => {
-        console.log(err)
-        res.status(500).json({ message: "Failed to add task." });
-      });
-  });
+  Tricks.addTasks(req.body, req.params.id)
+    .then(task => {
+      res.status(201).json(task);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Failed to add task." });
+    });
+});
 
+//get tasks per trick
+router.get("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
 
-  //get tasks per trick
-   router.get("/:id/tasks", async (req, res) => {
-   const { id } = req.params;
+  try {
+    const tasks = await Tasks.findTasks(id);
+    const transformedTasks = tasks.map(task => {
+      return {
+        ...task,
+        completed: task.completed === 0 ? false : true
+      };
+    });
+    res.status(200).json(transformedTasks);
+  } catch (error) {
+    res.status(500).json({ error: "Task not found" });
+  }
+});
 
-   try {
-     const tasks = await Tasks.findTasks(id);
-     const transformedTasks = tasks.map(task => {
-       return {
-         ...task,
-         completed: task.completed === 0 ? false : true
-       };
-     });
-     res.status(200).json(transformedTasks);
-   } catch (error) {
-     res.status(500).json({ error: "Task not found" });
-   }
- });
+// add a resource
+router.post("/", async (req, res) => {
+  const resourceData = req.body;
 
+  if (!req.body)
+    return res.status(400).json({
+      errorMessage: "Please provide needed info"
+    });
+  try {
+    const count = await Resources.add(resourceData);
+    res.status(201).json(count);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create new resource" });
+  }
+});
+
+//delete a resource
+router.delete("/:id/resources", async (req, res) => {
+  try {
+    console.log(Resources);
+    const ID = await Resources.remove(req.params.id);
+
+    if (ID > 0) {
+      res.status(200).json({ message: "resource has been deleted" });
+    } else {
+        console.log(404)
+      res.status(404).json({ message: "this resource can not be found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "unable to delete resource from db" });
+  }
+});
 
 module.exports = router;
--- resources/resource-router.js
+++ resources/resource-router.js
@@ -25,44 +25,44 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// add a resource
-router.post("/", async (req, res) => {
-  const resourceData = req.body;
+// // add a resource
+// router.post("/", async (req, res) => {
+//   const resourceData = req.body;
 
-  if (!req.body)
-    return res.status(400).json({
-      errorMessage: "Please provide needed info"
-    });
-  try {
-    const count = await Resources.add(resourceData);
-    res.status(201).json(count);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to create new resource" });
-  }
-});
+//   if (!req.body)
+//     return res.status(400).json({
+//       errorMessage: "Please provide needed info"
+//     });
+//   try {
+//     const count = await Resources.add(resourceData);
+//     res.status(201).json(count);
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to create new resource" });
+//   }
+// });
 
 
-//delete a resource 
-router.delete('/:id', async (req, res)=> {
-  try {
-    console.log(Resources)
-    const ID = await Resources.remove(req.params.id);
+// //delete a resource 
+// router.delete('/:id', async (req, res)=> {
+//   try {
+//     console.log(Resources)
+//     const ID = await Resources.remove(req.params.id);
     
-    if (ID > 0) {
+//     if (ID > 0) {
     
-      res
-        .status(200)
-        .json({ message: "resource has been deleted" });
-    } else {
-      res
-        .status(404)
-        .json({ message: "this resource can not be found" });
-    }
-  } catch (err) {
-    console.log(err);
-    res
-      .status(500)
-      .json({ message: "unable to delete resource from db" });
-  }
-})
+//       res
+//         .status(200)
+//         .json({ message: "resource has been deleted" });
+//     } else {
+//       res
+//         .status(404)
+//         .json({ message: "this resource can not be found" });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res
+//       .status(500)
+//       .json({ message: "unable to delete resource from db" });
+//   }
+// })
 module.exports = router;
